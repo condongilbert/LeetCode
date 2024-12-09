40df597f18740d1c9b8ac4508649eda9fca9749b
Gilbert <condongilbert@gmail.com>
2024-12-09 08:11:58 -0800

    operators

---
+++ operators.py
@@ -0,0 +1,12 @@
+# Read two integers from STDIN
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+# Print the sum of the two numbers
+print(a + b)
+
+# Print the difference (a - b)
+print(a - b)
+
+# Print the product of the two numbers
+print(a * b)
