30946f4e784e816b219e925bf938b0febe2fee06
Gilbert <condongilbert@gmail.com>
2025-02-02 10:07:23 -0800

    updated stock leetcode problem

--- StockSell.py
+++ StockSell.py
@@ -1,11 +1,12 @@
-def maxProfit(prices):
-    min_price = float('inf')  # Initialize minimum price
-    max_profit = 0           # Initialize maximum profit
-
-    for price in prices:
-        if price < min_price:
-            min_price = price  # Update minimum price
-        else:
-            max_profit = max(max_profit, price - min_price)  # Calculate and update max profit
-
-    return max_profit
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        profit = 0
+        
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i]-prices[i-1]
+        return profit
+                
+                
+            
